14b0382c4115f3f303d87387e39e9df59c8f88f4
ailtonhs <ailtonhenrick@outlook.com>
2024-09-15 21:20:04 -0300

    Operadores

---
+++ 02-operadores/02-aula/programa.js
@@ -0,0 +1,173 @@
+//Operadores
+// const x = 4;
+// const y = 5;
+
+// const soma = x + y;
+
+// const subtracao = x - y;
+
+// const multiplicacao = x * y;
+
+// const divisao = x / y;
+
+// const restoDeDivisao = y % x;
+
+
+
+
+// console.log(soma);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(restoDeDivisao);
+
+
+
+//Incrementação
+// let z = 1;
+
+// z++; //z += 1; //z = z + 1;
+// console.log(z);
+
+// //Decrementação
+// let a = 10;
+// a--; //a -= 1; //a = a - 1;
+
+// console.log(a);
+
+
+// let c = 10;
+// c *= 2;
+// console.log(c);
+
+// let d = 90;
+// d /= 3;
+
+// console.log(d);
+
+// let e = 9;
+// e %= 2;
+// console.log(e);
+
+
+/*Operadores Aritméticos
+
++ adição
+- subtração
+* multiplicação
+/ divisão
+% resto da divisão
+++ incrementar
+-- decrementar
+ */
+
+
+/*Operadores de Comparação
+
+> maior do que
+< menor do que
+>= maior ou igual a
+<= menor ou igual a
+== igual
+!= diferente
+*/
+
+// const a = 1;
+// const b = 2;
+
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a <= b);
+// console.log(a >= b);
+// console.log(a == b);
+// console.log(a != b);
+
+/*Operadores Lógicos
+
+&& E
+|| ou
+! Não (reverte o valor)
+
+&& (e)                   Resultado
+
+true && true                true
+true && false               false
+false && true               false
+false && false              false
+------------------------------------------
+
+const a = 1;
+const b = 2;
+console.log(a < b && a > b); //false
+console.log(a < b && a != b); //true
+console.log(a == b && a > b); //false
+/*
+
+
+
+/*Operador ||
+
+|| (ou)                    Resultado
+
+true || true                   true
+true || false                  true
+false || true                  true
+false || false                 false
+
+
+
+const a = 1;
+const b = 2;
+
+console.log(a < b || a > b);//true
+console.log(a == b || a != b);//true
+console.log(a == b || a > b);//false
+
+*/
+
+/* Operador !
+
+! (não)                     Resultado
+
+!true                          false
+!false                         true
+*/
+
+
+const a = 1;
+const b = 2;
+
+console.log(! (a == b));//true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
