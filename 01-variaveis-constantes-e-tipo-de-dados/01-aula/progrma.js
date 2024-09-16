96f10efd5b971c7beaf7a823081d705f66c1b5b3
ailtonhs <ailtonhenrick@outlook.com>
2024-09-15 21:18:26 -0300

    Variáveis, constantes, e tipos de dados

---
+++ 01-variaveis-constantes-e-tipo-de-dados/01-aula/progrma.js
@@ -0,0 +1,65 @@
+const x = 10; // Number
+const y = 2.5; // Number
+const nome = "Pedro"; // String
+const sobrenome = 'Souza'; //String
+
+const verdadeiro = true; //Boolean
+
+const falso = false; //Boolean
+
+const nulo = null; //nulo
+
+const nao_definido = undefined; //undefined
+
+
+// comando pra saber o tipo de dados de uma variáveis
+
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof nome);
+// console.log(typeof sobrenome);
+// console.log(typeof verdadeiro);
+// console.log(typeof falso);
+// console.log(typeof nulo);
+// console.log(typeof nao_definido);
+
+
+
+/* Regras para identificadores
+
+.Indetificadores devem começar com letra, um sinal de cifrão ($), ou um sinal de sublinhado(_).
+
+.Identificadores devem ser compostos de letras, números, sinal de cifrão ($), ou sinal de sublinhado (_).
+
+.Caracteres unicode são permitidos .
+
+.Identificadores não podem ser palavras reservadas, ou seja, comando do JavaScript.
+
+*/
+
+/*Variáveis, Constantes, e tipos de Dados
+
+Declarando variável e constante:
+let
+const
+
+Tipos de Dados:
+string
+number
+boolean
+null
+undefined
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
